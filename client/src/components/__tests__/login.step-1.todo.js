caa8a0f6af068e744a0fd50a4682e70e0d59af16
yerx <yer.x.xiong@gmail.com>
2020-03-22 17:00:30 -0400

    wrote jest test for logn step-1

--- client/src/components/__tests__/login.step-1.todo.js
+++ client/src/components/__tests__/login.step-1.todo.js
@@ -1,22 +1,42 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Login from '../login'
+
 // Basic unit test
 test('calls onSubmit with the username and password when submitted', () => {
   // Arrange
   // create a fake object to hold the form field values (username and password)
+  // const fakeUser = { username: 'chuck norris', password: 'something'}
   // create a jest.fn() for your submit handler
+  const handleSubmit = jest.fn()
   // render the Login component to a div
+  const div = document.createElement('div')
+  ReactDOM.render(<Login onSubmit={handleSubmit} />, div);
   // TIP: const div = document.createElement('div')
   //
   // get the field nodes
   // TIP: const inputs = div.querySelectorAll('input')
+  const inputs = div.querySelectorAll('input')
   // TIP: const form = div.querySelector('form')
+  const form = div.querySelector('form')
+  const { username, password } = form.elements;
+
   // fill in the field values
+  username.value = 'chuck';
+  password.value = 'norris'
   //
   // Act
   // submit the form:
+  form.dispatchEvent(new window.Event('submit'))
   // TIP: formNode.dispatchEvent(new window.Event('submit'))
   //
   // Assert
   // ensure your submit handler was called properly
+  expect(handleSubmit).toHaveBeenCalledTimes(1)
+  expect(handleSubmit).toHaveBeenCalledWith({
+    username: username.value,
+    password: password.value,
+  })
 })
 
 //////// Elaboration & Feedback /////////
